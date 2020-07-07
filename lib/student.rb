3b0c5b25635ced57675df0c1b585c568fcc15cb8
Raul Sposito <raulsposito@gmail.com>
2020-07-07 17:49:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,8 +9,8 @@ class Student
     @grade = grade
   end
 
-  def self.create_table 
-    sql = <<-SQL 
+  def self.create_table
+    sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
       id INTEGER PRIMARY KEY,
       name TEXT,
