6539319dbf00a634933288ef72ca22707d42058d
Raul Sposito <raulsposito@gmail.com>
2020-07-07 17:47:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,10 +6,11 @@ class Student
   def initialize(name, grade, id = nil)
     @name = name
     @grade = grade
-
   end
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+
+  
 
 end
